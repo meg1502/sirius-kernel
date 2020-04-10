2f0db88590519fdb2ca9e70fb7d420697d948cfa
Sascha Bieberstein <sbi@scireum.de>
2020-04-10 17:10:57 +0200

    Updates test suite to include junit 5 tests
    Fixes: MISC-3

--- src/test/java/TestSuite.java
+++ src/test/java/TestSuite.java
@@ -6,23 +6,29 @@
  * http://www.scireum.de - info@scireum.de
  */
 
-import com.googlecode.junittoolbox.SuiteClasses;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.runner.RunWith;
-import sirius.kernel.ScenarioSuite;
 import sirius.kernel.TestHelper;
 
-@RunWith(ScenarioSuite.class)
-@SuiteClasses({"**/*Test.class", "**/*Spec.class"})
+@RunWith(JUnitPlatform.class)
+@IncludeClassNamePatterns({"^.*Test$", ".^.*Spec$"})
+@SelectPackages("sirius")
 public class TestSuite {
 
     @BeforeClass
+    @BeforeAll
     public static void setUp() {
         TestHelper.setUp(TestSuite.class);
     }
 
     @AfterClass
+    @AfterAll
     public static void tearDown() {
         TestHelper.tearDown(TestSuite.class);
     }
